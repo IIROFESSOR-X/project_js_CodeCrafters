6b03abccc8d67b5d19532dd9f9d0f98b7c25ee06
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-01-26 18:52:00 +0200

    fetch form

--- src/js/footer-form.js
+++ src/js/footer-form.js
@@ -1,46 +1,70 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-document.querySelector(".footer-form").addEventListener("submit", 
-function(event) {
+document
+  .querySelector('.footer-form')
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
     const emailInput = this.elements.email;
-  
     const emailValue = emailInput.value.trim();
 
     if (!emailValue) {
-        alert('Field must be filled in');
-        return; 
-      }
-    
-      const formData = {
-        email: emailValue
-      };
-    });
+      alert('Field must be filled in');
+      return;
+    }
 
-// try {
-//     const response = await fetch(), {
-//         method:'POST',
-//         headers: {'Content-Type':'application/json'},
-//         body: JSON.stringify(formData)
-//     });
+    const formData = { email: emailValue };
 
-//     if (!response.ok) {
-//         throw new Error('Send ok');
-//     }
+    fetch('https://energyflow.b.goit.study/api/subscription', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.success) {
+          iziToast.success({
+            position: 'topRight',
+            message: 'Subscription successful!',
+          });
+          this.reset();
+        } else {
+          throw new Error('Subscription failed: ' + data.error);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to subscribe. Please try again later.');
+      });
+  });
 
-//         const data = await response.json();
+//             const state = this.elements['state'].value;
 
-//         if (data.success) {
-//             alert ('Subscription successful!');
-//             this.reset(); 
-//         } else {
-//             throw new
-//             Error('Subscription failed: ' + data.error);
-//         }
-        
-//      } catch (error) {
-//     console.error()
-//     alert('Failed to subscribe: ' + error.message);
-// }
-    
-// });
+//             const notificationPromise = new Promise((resolve, reject) => {
+//               setTimeout(() => {
+//                   if (state === emailInput) {
+//                     resolve(delay);
+//                   } else {
+//                     reject(delay);
+//                   }
+//                 }, delay);
+//               });
+
+//             notificationPromise
+//               .then(() => {
+//                 iziToast.success({ message: `Successfully!` });
+
+//               })
+//               .catch(() => {
+//                 iziToast.error({ message: `Not successful!` });
+
+//               });
+//               this.reset();
+
+//         });
---
+++ src/main.js
@@ -0,0 +1,50 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+document.querySelector(".footer-form").addEventListener("submit",
+function(event) {
+    event.preventDefault();
+
+    const emailInput = this.elements.email;
+    const emailValue = emailInput.value.trim();
+
+    if (!emailValue) {
+        alert('Field must be filled in');
+        return; 
+      }
+    
+      const formData = {email: emailValue};
+
+      fetch("https://energyflow.b.goit.study/api/subscription", {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(formData)
+      })
+      .then((response) => {
+        if (!response.ok) {
+      throw new Error('Network response was not ok');
+                }
+          return response.json();
+            })
+     .then(data => {
+          if (data.success) {
+            iziToast.success({
+                position: 'topRight', 
+                message:'Subscription successful!'});
+                        this.reset(); 
+                    } else {
+        throw new Error('Subscription failed: ' + data.error);
+                }            
+                    })
+    .catch(error => {
+               console.error('Error:', error);
+                alert('Failed to subscribe. Please try again later.');
+                    });
+            
+
+
+
+     
+    });
