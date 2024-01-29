d7f95a3bdc20d58ec047d10302c215f91b3b1d9c
IIROFESSOR-X <141030664+IIROFESSOR-X@users.noreply.github.com>
2024-01-29 08:11:28 +0000

    Deploying to gh-pages from @ IIROFESSOR-X/project_js_CodeCrafters@a16f89e1a69b587cf983c655da6ae48da83061d2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,18 +1,18 @@
-import"./assets/quote-section-140dfb8b.js";import"./assets/vendor-0cb09735.js";let u=localStorage.getItem("favorites"),e;const t={favoritesList:document.querySelector(".favorites-list"),gymInfo:document.querySelector(".gym-info")};window.addEventListener("load",v);t.favoritesList.addEventListener("click",p);function f(){try{e=JSON.parse(u)}catch(s){console.log(s),e=[]}return e}function p(s){if(s.target.nodeName!=="BUTTON"||s.target.dataset.action!=="delete")return;const r=s.target.dataset.id,a=e.find(({_id:n})=>n===r),i=e.indexOf(a);e.splice(i,1),localStorage.setItem("favorites",JSON.stringify(e)),d()}function v(){f(),e.length?(t.gymInfo.classList.add("is-hidden"),d()):t.gymInfo.classList.remove("is-hidden")}function d(){const s=e.reduce((r,{_id:a,rating:i,name:n,burnedCalories:o,time:c,bodyPart:l,target:g})=>r+`<li class="exer-card-item">
+import{o as u}from"./assets/quote-section-140dfb8b.js";import"./assets/vendor-0cb09735.js";let d,a;const r={favoritesList:document.querySelector(".favorites-list"),gymInfo:document.querySelector(".gym-info")};window.addEventListener("load",h);r.favoritesList.addEventListener("click",m);r.favoritesList.addEventListener("click",p);function p(t){if(t.target.dataset.action!=="start")return;const s=t.target.dataset.id;s&&u(s)}function v(){d=localStorage.getItem("favorites");try{a=JSON.parse(d)}catch(t){console.log(t),a=[]}}function m(t){if(t.target.dataset.action!=="delete")return;const s=t.target.dataset.id,e=a.find(({_id:n})=>n===s),i=a.indexOf(e);a.splice(i,1),localStorage.setItem("favorites",JSON.stringify(a)),a.length||r.gymInfo.classList.remove("is-hidden"),o()}function h(){v(),a.length?(r.gymInfo.classList.add("is-hidden"),o()):r.gymInfo.classList.remove("is-hidden")}function o(){const t=a.reduce((s,{_id:e,rating:i,name:n,burnedCalories:c,time:l,bodyPart:g,target:f})=>s+`<li class="exer-card-item">
   <div class="exer-card-background">
     <div class="card-workout-wrapper">
       <p class="exer-workout-text">WORKOUT</p>
-      <button class="card-btn-delete" data-action="delete" data-id="${a}">
-        <svg class="card-btn-delete-svg" width="16" height="16">
-          <use xlink:href="/img/symbol-defs.svg#icon-trash"></use>
+      <button class="card-btn-delete" data-action="delete" data-id="${e}">
+        <svg class="card-btn-delete-svg" width="16" height="16" data-action="delete" data-id="${e}"">
+          <use xlink:href="/img/symbol-defs.svg#icon-trash" data-action="delete" data-id="${e}"></use>
         </svg>
       </button>
-      <button class="card-start-button" data-id="${a}">
+      <button class="card-start-button" data-action="start" data-id="${e}">
         Start
-        <svg class="card-arrow-svg" width="14" height="14" data-id="${a}">
+        <svg class="card-arrow-svg" width="14" height="14" data-action="start" data-id="${e}">
           <use
-            xlink:href="/img/symbol-defs.svg#icon-arrow"
-            data-id="${a}"
+            xlink:href="/img/symbol-defs.svg#icon-arrow" data-action="start"
+            data-id="${e}"
           ></use>
         </svg>
       </button>
@@ -26,18 +26,18 @@ import"./assets/quote-section-140dfb8b.js";import"./assets/vendor-0cb09735.js";l
     <p class="card-burned-calories-text">
       Burned calories:
       <span class="card-burned-calories-span"
-        >${o} / ${c} min</span
+        >${c} / ${l} min</span
       >
     </p>
 
     <p class="card-body-part-text">
       Body part:
-      <span class="card-burned-calories-span">${l}</span>
+      <span class="card-burned-calories-span">${g}</span>
     </p>
     <p class="card-target-text">
       Target:
-      <span class="card-burned-calories-span">${g}</span>
+      <span class="card-burned-calories-span">${f}</span>
     </p>
   </div>
-</li>`,"");t.favoritesList.innerHTML=s}
+</li>`,"");r.favoritesList.innerHTML=t}
 //# sourceMappingURL=commonHelpers2.js.map
