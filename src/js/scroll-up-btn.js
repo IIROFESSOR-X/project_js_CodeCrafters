c26e0f27ded237f330f3f3b2e082d67f9611dc6a
Martin <martinmyhalyna@gmail.com>
2024-01-30 12:22:31 -0500

    scroll top upd

--- src/js/scroll-up-btn.js
+++ src/js/scroll-up-btn.js
@@ -29,32 +29,3 @@ function scrollingTop() {
 }
 
 scrollingTop();
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Отримуємо посилання на кнопку
-//   let scrollToTopBtn = document.querySelector('.scroll-up-btn');
-
-//   // Додаємо подію click на кнопку
-//   scrollToTopBtn.addEventListener('click', scrollToTop);
-
-//   // Перевірка прокрутки сторінки
-//   // window.onscroll = function () {
-//   //   showScrollButton();
-//   // };
-
-//   // function showScrollButton() {
-//   //   // Показати або приховати кнопку в залежності від положення прокрутки
-//   //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//   //     scrollToTopBtn.style.display = 'block';
-//   //   } else {
-//   //     scrollToTopBtn.style.display = 'none';
-//   //   }
-//   // }
-
-//   function scrollToTop() {
-//     // Анімовано прокрутити сторінку на початок
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth',
-//     });
-//   }
-// });
