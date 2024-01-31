0f02b39d60187928d452bb71b97879df181a98d5
Vasyl Diachuk <djachukvasja@gmail.com>
2024-01-31 17:56:37 +0200

    1

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,7 +1,8 @@
 import { openOnClick } from './modal-window';
-import  icons  from '../img/symbol-defs.svg';
+import icons from '../img/symbol-defs.svg';
 let savedCards;
 let parsedCards;
+let currentPageFavorite = 1;
 const ref = {
   favoritesList: document.querySelector('.favorites-list'),
   gymInfo: document.querySelector('.gym-info'),
@@ -11,12 +12,11 @@ const ref = {
 window.addEventListener('load', windowLoad);
 
 export const renderFavorites = parsedData => {
+  if (document.body.clientWidth <= 768 && parsedCards.length > 0)
+    parsedData = makePaginationOfCards();
   if (ref.favoritesList) {
     const favoritesCardString = parsedData.reduce(
-      (
-        html,
-        { _id, name: names, burnedCalories, time, bodyPart, target }
-      ) => {
+      (html, { _id, name: names, burnedCalories, time, bodyPart, target }) => {
         return (
           html +
           `<li class="exer-card-item">
@@ -67,14 +67,14 @@ export const renderFavorites = parsedData => {
     );
 
     ref.favoritesList.innerHTML = favoritesCardString;
-  };
-}
+  }
+};
 
 function parsingCards() {
   savedCards = localStorage.getItem('favorites');
   try {
     parsedCards = JSON.parse(savedCards);
-    if (!parsedCards) parsedCards = []; 
+    if (!parsedCards) parsedCards = [];
   } catch (error) {
     console.log(error);
     parsedCards = [];
@@ -82,15 +82,14 @@ function parsingCards() {
 }
 
 function onDeleteBtnClick(event) {
-
   if (event.target.dataset.action !== 'delete') {
     return;
   }
   parsingCards();
   const idCardforDelete = event.target.dataset.id;
-  // console.log(idCardforDelete);
+
   const filtredCards = parsedCards.filter(({ _id }) => _id !== idCardforDelete);
-  // console.log(filtredCards);
+
   localStorage.setItem('favorites', JSON.stringify(filtredCards));
   parsingCards();
   if (!parsedCards.length) {
@@ -109,12 +108,13 @@ function onStartBtnClick(event) {
     openOnClick(idCardforStart);
   }
 }
-
+const containerForPages = document.querySelector(`.container-for-pagination`);
 function windowLoad() {
   parsingCards();
-  if (parsedCards.length && ref.gymInfo ) {
+  if (parsedCards.length && ref.gymInfo) {
     ref.gymInfo.classList.add('is-hidden');
     renderFavorites(parsedCards);
+    containerForPages.addEventListener(`click`, userChangePage);
     ref.favoritesList.addEventListener('click', onDeleteBtnClick);
     ref.favoritesList.addEventListener('click', onStartBtnClick);
   } else if (ref.gymInfo) {
@@ -124,4 +124,83 @@ function windowLoad() {
   }
 }
 
+// containerForPages.addEventListener(`click`, userChangePage);
+const scroltoSection = document.querySelector(`.exercises-section-fv`);
+function userChangePage(e) {
+  let numberPage = e.target.dataset.number;
+  if (numberPage === currentPageFavorite) return;
+  currentPageFavorite = numberPage;
+  parsingCards();
+  renderFavorites(parsedCards);
+}
 
+const makePaginationOfCards = () => {
+  let totalPages = parsedCards.length;
+  let index = currentPageFavorite * 8;
+
+  containerForPages.innerHTML = formatNumericOfPages(
+    currentPageFavorite,
+    Math.ceil(totalPages / 8)
+  );
+
+  scroltoSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  return parsedCards.slice(index - 8, index);
+};
+
+function formatNumericOfPages(cPage, tPage) {
+  cPage = Number.parseInt(cPage);
+
+  let leftErrow = `<svg  class="page-choice-svg"  width="18"  height="18"    data-id="left"  
+                      xmlns="http://www.w3.org/2000/svg"  id="Outline"  viewBox="0 0 24 24"  width="512"  height="512">  <path    d="M17.17,24a1,1,0,0,1-.71-.29L8.29,15.54a5,5,0,0,1,0-7.08L16.46.29a1,1,0,1,1,1.42,1.42L9.71,9.88a3,3,0,0,0,0,4.24l8.17,8.17a1,1,0,0,1,0,1.42A1,1,0,0,1,17.17,24Z"
+                      />
+                    </svg> `;
+  let rightErrow = `<svg class="page-choice-svg" data-id="right"   xmlns="http://www.w3.org/2000/svg" id="Outline"
+                        viewBox="0 0 24 24" width="512" height="512"><path d="M7,24a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42l8.17-8.17a3,3,0,0,0,0-4.24L6.29,1.71A1,1,0,0,1,7.71.29l8.17,8.17a5,5,0,0,1,0,7.08L7.71,23.71A1,1,0,0,1,7,24Z" /></svg> `;
+  let start = 0,
+    resultHtml = ``,
+    end = 0;
+
+  if (tPage < 6) {
+    start = 1;
+    end = tPage + 1;
+    return addNumeric(start, end);
+  }
+  if (tPage > 5 && cPage > 0 && cPage < 4) {
+    end = 5 + 1;
+    resultHtml = addNumeric(start + 1, end) + rightErrow;
+    return resultHtml;
+  }
+
+  if (cPage > 3 && cPage + 2 < tPage) {
+    start = cPage - 2;
+    end = cPage + 3;
+
+    return leftErrow + addNumeric(start, end) + rightErrow;
+  }
+  if (tPage <= cPage + 2) {
+    tPage == cPage + 2
+      ? (start = cPage - 2)
+      : tPage - cPage == 1
+      ? (start = cPage - 3)
+      : (start = cPage - 4);
+    return leftErrow + addNumeric(start, tPage + 1);
+  }
+  function addNumeric(s, e) {
+    let r = ``;
+    for (let i = s; i < e; i++) {
+      if (cPage == i) {
+        r += `<li>
+          <button class="exercises-pages-button select-pages-ative" 
+           data-number="${i}" >${i}</button>
+        </li>`;
+      }
+      if (cPage != i) {
+        r += `<li>
+          <button class="exercises-pages-button" 
+           data-number="${i}" >${i}</button>
+        </li>`;
+      }
+    }
+    return r;
+  }
+}
